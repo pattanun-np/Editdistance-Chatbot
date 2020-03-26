6a4ad148182d75bd55e82698dc881351b7a3ff7b
pattanunNP <phattanun@hotmail.com>
2020-03-27 03:21:30 +0700

    test_edt

--- db_connect.py
+++ db_connect.py
@@ -6,14 +6,14 @@ chatbot_db = client["heroku_7hhcq7v4"]
 
 def read_data(collection_name):## อ่านข้้อมูลจาก Database นะครับ
      compare_text =[]
-     sentiment_text=[] # สร้าง list ไว้เปรียบเทียบ
+     intent_text=[] # สร้าง list ไว้เปรียบเทียบ
      my_collection = chatbot_db[collection_name] #เรียก coloection 
      for all_data in my_collection.find(): #เรียก data ทั้ง db
         keyword_data = all_data['keyword']
         intent_data = all_data['intent']
         compare_text.append(keyword_data)
         sentiment_text.append(intent_data)
-     return compare_text,  intent_data
+     return compare_text,  intent_text
 
 def send_data(collection_name,data):## collection ที่ต้องการส่ง และข้อมูล
     my_collection = chatbot_db[collection_name] #เรียก coloection 
